43dff5d9c6d0c03a899a9990a2ec2c562a2d4e85
Jose Rodrigo <acg_1349@hotmail.com>
2022-01-08 18:16:39 -0500

    Se modifico el titulo en el componente GifExpertApp.js

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -9,7 +9,7 @@ export const GifExpertApp = () => {
 
     return (
         <>
-            <h2>GifExpertApp</h2>
+            <h2>GifExpertApp :)</h2>
             <AddCategory setCategories={setCategories} />
             <hr />
 
