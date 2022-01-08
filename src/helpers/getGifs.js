ea90ddf6b673209a03d8b1f06e112894dccd5c60
Jose Rodrigo <acg_1349@hotmail.com>
2022-01-08 17:20:39 -0500

    url fetch modificado

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 
 export const getGifs = async (category) => {
 
-    const URL = `api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=bhyQRbVDHWZWJe5tlg4a28nCDbRFz1og`;
+    const URL = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=bhyQRbVDHWZWJe5tlg4a28nCDbRFz1og`;
 
     const respuesta = await fetch(URL);
     const { data } = await respuesta.json();
